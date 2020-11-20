4b46333e50710a187d741184b9bd52f7bc22c06c
Damir D <damir180501@mail.ru>
2020-11-20 21:33:08 +0300

    Initial commit

---
+++ desktop/src/ru/itlab/lasthero/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ru.itlab.lasthero.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.itlab.lasthero.MainActivity;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainActivity(), config);
+	}
+}

33c3dcbb472f5703a05f5208205e571d312579c0
KoaLa1101 <rproba@list.ru>
2020-12-13 16:18:58 +0300

    Lobbies fixed

--- src/main/java/Server/Server.java
+++ src/main/java/Server/Server.java
@@ -2,7 +2,6 @@ package Server;
 
 import java.io.*;
 import java.net.ServerSocket;
-import java.net.Socket;
 import java.util.ArrayList;
 
 public class Server {
@@ -11,7 +10,7 @@ public class Server {
 
     public static void main(String[] args) {
         try {
-            System.out.println("Server. Server is trying to start..");
+            System.out.println("Server. Server was started..");
             serverSocket = new ServerSocket(Protocol.GAME_PORT);
             users = new ArrayList<User>();
             while (true) {
@@ -22,30 +21,8 @@ public class Server {
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
-            System.out.println("Server was closed..");
+            System.out.println("Server. Server was closed..");
         }
     }
 
 }
-
-
-/* public static void main(String[] args) {
-        try {
-            System.out.println("Server.Server starts...");
-            ServerSocket server = new ServerSocket(Protocol.PORT);
-            Socket socket = server.accept();
-            ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
-            System.out.println("User connected!");
-            String data = "";
-            while (true) {
-                data = (String) in.readObject();
-                Message message = Message.setSentMessage(data);
-                System.out.println(message.getX() + " " + message.getY());
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-    }
- */
